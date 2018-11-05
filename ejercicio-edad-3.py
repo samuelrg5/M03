6cb5f70b33f0cc21eb4b5d6aed49cf688ee787e9
samuelrg5 <44439107+samuelrg5@users.noreply.github.com>
2018-11-05 11:33:21 +0100

    Update ejercicio-edad-3.py

--- ejercicio-edad-3.py
+++ ejercicio-edad-3.py
@@ -1,3 +1,5 @@
-edad=int(input("indique su edad:"))
-if (((edad>=18)and(edad<=23))or(edad==17)):
-    print("puedes entrar en la sesión de jóvenes.")
+edad=int(input("Indique su edad:"))
+if ((edad>=18)and(edad<=23)or(edad==17)):
+    print("Puedes entrar en la sesión de jóvenes.")
+else:
+    print("Lo siento no puedes pasar")
